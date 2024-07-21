d3ee657e2198d4584bb6b41a9effec34d0a7381e
Naveen John <65065036+naveenjohn942-prog@users.noreply.github.com>
2024-07-21 22:17:31 +0530

    Create SortColors.java

---
+++ SortColors.java
@@ -0,0 +1,49 @@
+class SortColors {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int[] sorted = mergeSort(nums);
+        System.arraycopy(sorted, 0, nums, 0, nums.length);
+    }
+    int[] mergeSort(int[] arr){
+        if(arr.length==1){
+            return arr;
+        }
+        int mid=arr.length/2;
+        int[] left=mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int[] right=mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+
+        return merge(left,right);
+    }
+    int[] merge(int[] left,int[] right){
+        int[] mix=new int[left.length+right.length];
+
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<left.length&&j<right.length){
+            if(left[i]<right[j]){
+                mix[k]=left[i];
+                i++;
+            }else{
+                mix[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<left.length){
+            mix[k]=left[i];
+            i++;
+            k++;
+        }
+        
+        while(j<right.length){
+            mix[k]=right[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
