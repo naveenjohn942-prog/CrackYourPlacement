81a72b813e8d46ae9f883b4954497faf916e5270
Naveen John <65065036+naveenjohn942-prog@users.noreply.github.com>
2024-07-30 18:56:15 +0530

    Create MiddleNode.java

---
+++ MiddleNode.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        ListNode first=head;
+        ListNode second=head;
+        while(second!=null&& second.next!=null){
+            first=first.next;
+            second=second.next.next;
+            
+        }
+        return first;
+    }
+}
